3106cb91f3011f4afc917882be36548914efa80a
Gireesh45 <kudupudivenkata95@gmail.com>
2026-01-01 10:54:24 +0530

    fix: explicitly set Vite HTML entry for CI

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config///
-// export default defineConfig({
-//   plugins: [react()],
-//   publicDir: 'public',
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url)),
-//     },
-//   },
-// })
-
-
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
-
